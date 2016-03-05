fd1d45d7e19693c33568543116cb0696058aa6a8
lion17s <serhii.sheptytskyi@gmail.com>
2016-03-06 01:00:40 +0200

    Added HomePage for SalesForce

---
+++ BPP/salesforce/pages/HomePage.java
@@ -0,0 +1,11 @@
+package salesforce.pages;
+
+import org.openqa.selenium.By;
+
+public class HomePage {
+	
+	public static By userLabel = By.id("userNavLabel"),
+			advancedSearch = By.linkText("Advanced Search..."),
+			homeTab = By.linkText("Home");
+
+}
