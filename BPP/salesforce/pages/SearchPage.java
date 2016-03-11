a114d5d5bae2109800c10e39631c29554882301c
lion17s <serhii.sheptytskyi@gmail.com>
2016-03-11 15:38:44 +0200

    Added SearchPage to Sales Force package

--- BPP/salesforce/pages/SearchPage.java
+++ BPP/salesforce/pages/SearchPage.java
@@ -1,33 +1,43 @@
 package salesforce.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import com.relevantcodes.extentreports.LogStatus;
 import utils.MainClass;
 
 public class SearchPage extends MainClass {
-	
-	//Links
+
+	// Links
 	public static By deselectAll = By.linkText("Deselect All");
-	
-	//Check Boxes
+
+	// Check Boxes
 	public static By leadChkbox = By.id("00Q");
-	
-	//Fields
+
+	// Fields
 	public static By searchField = By.id("str");
-	
-	//Buttons
+
+	// Buttons
 	public static By searchButton = By.name("search");
-	
-	
-	//Search method
-	public static void searchRecord(String recordType, String name){
+
+	// Search method
+	public static void searchRecord(String recordType, String fNameLname, String email, String phone) {
+		Logger().log(LogStatus.INFO, "Trying to search " + recordType + "with: " + fNameLname + " " + email + " "
+				+ phone + " in SalesForce");
 		clickOn(HomePage.homeTab, "Home Tab");
 		clickOn(HomePage.advancedSearch, "Advanced Search Link");
 		clickOn(SearchPage.deselectAll, "Deselect All Link");
-		switch (recordType){
+		switch (recordType) {
 		case "Leads":
 			clickOn(SearchPage.leadChkbox, "Lead Checkbox");
-			enterText(SearchPage.searchField, name);
-			clickOn(SearchPage.searchButton, "Search Button");
+			enterText(SearchPage.searchField, fNameLname);
+			getElement(SearchPage.searchField).sendKeys(Keys.RETURN);
+			try {
+				Driver().findElement(By.id("Lead_body"));
+				clickOn(By.xpath("//a[text()='" + email + "']/../preceding-sibling::td[text()='" + phone
+						+ "']/preceding-sibling::*/a[text()='" + fNameLname + "']"), fNameLname + " link");
+			} catch (Exception e) {
+			}
 			break;
 		}
 	}
