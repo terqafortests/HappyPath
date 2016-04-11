395124237148a0ea1f361bbe4f92c251da4e607b
lion17s <serhii.sheptytskyi@gmail.com>
2016-04-11 16:17:00 +0300

    Updated this test

--- BPP/salesforce/testNGTests/SubmitApplicationTest.java
+++ BPP/salesforce/testNGTests/SubmitApplicationTest.java
@@ -13,16 +13,16 @@ public class SubmitApplicationTest extends MainClass {
 	@Test(testName = "Submit application", description = "Verifies if application was submitted", groups = {
 			"Happy Path" })
 	public void submitApplicationTest() {
-		clickOn(By.xpath("//td[@id='topButtonRow']/input[@title='Submit Application']"), "Submit Application Button");
 		try {
-			waitForAlert();
-			Logger().log(LogStatus.FAIL, "Possibly duplicate leads");
-			Assert.fail();
-		} catch (Exception e) {
+			clickOn(By.xpath("//td[@id='topButtonRow']/input[@title='Submit Application']"), "Submit Application Button");
 			getPage(ConvertLeadToStudentTest.address);
 			bannerID = getElementText(By.xpath("//td[text()='Banner ID']/following-sibling::td[1]//div"));
 			System.out.println(bannerID);
-			assertTrue("Verifying if application was submitted(Banner ID was created)", bannerID.length() == 9);
+			assertTrue("Verifying if application was submitted(Banner ID was created)", bannerID.length() == 9);			
+		} catch (Exception e) {
+            waitForAlert();
+			Logger().log(LogStatus.FAIL, "Possibly duplicate leads");
+			Assert.fail(e.getCause().toString());
 		}
 	}
 
